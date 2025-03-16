ba4e748eb6f31f9038337fe33975fb4fbcab3900
Katja <katimailkaaa@gmail.com>
2025-03-16 14:27:22 +0300

    Результат отбора

--- rezult16/main.py
+++ rezult16/main.py
@@ -1,23 +1,17 @@
-from flask import Flask, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/choice/<planet_name>')
-def choice(planet_name):
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
     spisok = [
-        planet_name,
-        'На неё много необходимых ресурсов;',
+        f'Претендента на участие в миссии {nickname}:',
+        f'Поздравляем! Ваш рейтинг после {level} этапа отбора составляет {rating}!',
 
-        'На ней есть вода и атмосфера;',
-
-        'На ней есть небольшое магнитное поле;',
-
-        'Наконец, она просто красива!;',
+        'Желаем удачи!',
 
     ]
-    url_pic = url_for('static', filename='img/MARS.png')
-    url_style = url_for('static', filename='css/style.css')
     return """<!doctype html>
         <html lang="en">
         <head>
@@ -28,22 +22,16 @@ def choice(planet_name):
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-            
-            <title>Колонизация</title>
+
+            <title>Результаты</title>
         </head>
         <body>
-        <h1>Моё предложение: {}</h1>
-        <h3>Эта планета близка к Земле:</h3>
-        <div class="alert alert-primary" role="alert">
-          <h3>{}</h3>
-        </div>
-        <div class="alert alert-secondary" role="alert">
-          <h3>{}</h3>
-        </div>
+        <h1>Результаты отбора</h1>
+        <h3>{}</h3>
         <div class="alert alert-success" role="alert">
           <h3>{}</h3>
         </div>
-        <div class="alert alert-danger" role="alert">
+        <div class="alert alert-secondary" role="alert">
           <h3>{}</h3>
         </div>
         </body>
