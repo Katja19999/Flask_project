2dcda8d7fb4be429aa893fc4f0be500db23ca5f9
Katja <katimailkaaa@gmail.com>
2025-03-16 14:45:46 +0300

    Пейзажи марса

--- main.py
+++ main.py
@@ -107,6 +107,58 @@ def results(nickname, level, rating):
         </body>
         </html>""".format(*spisok)
 
+@app.route('/choice')
+def choice():
+    return """<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Галерея Марса</title>
+</head>
+<body>
+    <h1>Пейзажи Марса</h1>
+    <!-- Carousel wrapper -->
+    <div id="carouselBasicExample" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <!-- Indicators -->
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselBasicExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselBasicExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselBasicExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+
+        <!-- Inner -->
+        <div class="carousel-inner">
+            <!-- Single item -->
+            <div class="carousel-item active">
+                <img src="static/img/mars.webp" class="d-block w-100" alt="Sunset Over the City">
+            </div>
+            <!-- Single item -->
+            <div class="carousel-item">
+                <img src="static/img/mars2.jpg" class="d-block w-100" alt="Canyon at Night">
+            </div>
+            <!-- Single item -->
+            <div class="carousel-item">
+                <img src="static/img/mars3.jpg" class="d-block w-100" alt="Cliff Above a Stormy Sea">
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselBasicExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselBasicExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</body>
+</html>
+    """
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
