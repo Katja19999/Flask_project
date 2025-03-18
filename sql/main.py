b0fd40dfe5121a632caeef37bb1a49b4b5f62c79
Katja <katimailkaaa@gmail.com>
2025-03-18 20:17:05 +0300

    Первая работа

--- sql/main.py
+++ sql/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from data import db_session
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -51,6 +52,44 @@ def main():
     user.address = "module_2"
     user.email = "bibizjan1234@mars.org"
     session.add(user)
+
+    session.commit()
+
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = 2
+    job.start_date = datetime.datetime.now().date()
+    job.is_finished = False
+    session.add(job)
+
+    job = Jobs()
+    job.team_leader = 2
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 5
+    job.collaborators = 3
+    job.start_date = datetime.datetime.now().date()
+    job.is_finished = False
+    session.add(job)
+
+    job = Jobs()
+    job.team_leader = 3
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 2
+    job.collaborators = 2
+    job.start_date = datetime.datetime.now().date()
+    job.is_finished = False
+    session.add(job)
+
+    job = Jobs()
+    job.team_leader = 4
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 6
+    job.collaborators = 3
+    job.start_date = datetime.datetime.now().date()
+    job.is_finished = False
+    session.add(job)
 
     session.commit()
 
