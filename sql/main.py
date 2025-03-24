6a59d96ccbba2a4576dce57a69098d2b1ae549f3
Katja <katimailkaaa@gmail.com>
2025-03-24 12:13:15 +0300

    Журнал 2.0

--- sql/main.py
+++ sql/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from data import db_session
 import datetime
-
+import sqlalchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 from data.users import Users
@@ -14,8 +14,8 @@ def index():
     return render_template("index.html", jobs=jobs)
 
 def main():
-    db_session.global_init('db.mars.db')
-    # app.run()
+    db_session.global_init('db/mars.db')
+    app.run()
 
 
 
